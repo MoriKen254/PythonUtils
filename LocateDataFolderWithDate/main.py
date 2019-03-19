c45d221f965e3b7a5a4ef7c3e55483fe5dfbb229
Mori <p595201m@mail.kyutech.jp>
2019-03-19 23:31:00 +0900

    Modify to analyze from update date to capture date

--- LocateDataFolderWithDate/main.py
+++ LocateDataFolderWithDate/main.py
@@ -6,19 +6,25 @@ This software is released under the MIT License.
 See LICENSE file included in this repository.
 """
 import os
+from PIL import Image
 from datetime import datetime as dt
 import shutil
+import copy
+
 
 
 def main():
-    #dir_path = r'C:\Users\masam\PycharmProjects\MyUtils\LocateDateFolder\TestFiles'
-    dir_path = r'C:\Users\masam\Desktop\photos'
+    dir_path = r'C:\Users\masam\PycharmProjects\MyUtils\LocateDateFolder\TestFiles'
     os.chdir(dir_path)
 
     files = extract_file_names_in_dir(dir_path)
 
     for x in files:
-        dt_str = extract_date_str_from_file(x)
+        root, ext = os.path.splitext(x)
+        if ext == ".MOV" or ext == ".PNG":
+            dt_str = extract_date_str_from_file(x)
+        else:
+            dt_str = get_date(x)
 
         # create dir whose name is date if not existed
         if not os.path.exists(dt_str):
@@ -26,6 +32,7 @@ def main():
 
         # move the file to the dir
         if os.path.exists(x):
+            # shutil.copy2('./' + x, './' + dt_str + './' + x)
             shutil.move('./' + x, './' + dt_str)
 
 
@@ -39,11 +46,24 @@ def extract_file_names_in_dir(dir_path):
 
 
 def extract_date_str_from_file(file_name):
-    dt_array = dt.fromtimestamp(os.stat(file_name).st_mtime)
+    dt_array = dt.fromtimestamp(os.stat(file_name). st_mtime)
     dt_str = dt_array.strftime('%Y%m%d')
     dt_str = dt_str[2:]
 
     return dt_str
+
+
+def get_date(file_name):
+    image = Image.open(file_name)
+    exif = image._getexif()
+    #if not 0x9003 in exif: continue
+    time_str = exif[36867]
+    dt_str = time_str[2:10]
+    dt_str = dt_str.replace(":", "")
+    dt_str_deep = copy.deepcopy(dt_str)
+    image.close()
+
+    return dt_str_deep
 
 
 if __name__ == '__main__':
